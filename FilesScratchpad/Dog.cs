1b8d9aae09a9311dff0d07769ed4c1e1d0143b55
Eric Ray <erichamion@gmail.com>
2022-04-21 19:36:39 -0500

    Add project files.

---
+++ FilesScratchpad/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesScratchpad
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public Person Owner { get; set; }
+
+        public string Serialize()
+        {
+            return $"{Name}<-->{Owner.Name}";
+        }
+
+        public static Dog Deserialize(string dogString)
+        {
+            string[] parts = dogString.Split("<-->");
+            Dog result = new Dog();
+            result.Name = parts[0];
+            Person owner = new Person();
+            owner.Name = parts[1];
+            result.Owner = owner;
+            return result;
+        }
+    }
+}
---
+++ FilesScratchpad/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+
+using FilesScratchpad;
+using System.Text.Json;
+
+//Person fred = new Person();
+//fred.Name = "Fred";
+//Person wilma = new Person();
+//wilma.Name = "Wilma";
+
+//Dog rex = new Dog();
+//rex.Owner = fred;
+//rex.Name = "Rex";
+
+//Dog spot = new Dog();
+//spot.Owner = wilma;
+//spot.Name = "Spot";
+
+//List<Dog> dogs = new List<Dog> { rex, spot};
+
+//JsonSerializerOptions options = new JsonSerializerOptions
+//{
+//    WriteIndented = true,
+//};
+//string dogsJson = JsonSerializer.Serialize(dogs, options);
+
+//using (FileStream fs = File.OpenWrite(@"C:\Users\erich\dogs.json"))
+//using (TextWriter writer = new StreamWriter(fs))
+//{
+//    writer.Write(dogsJson);
+//}
+
+string dogsJson = File.ReadAllText(@"C:\Users\erich\dogs.json");
+List<Dog> dogs = JsonSerializer.Deserialize<List<Dog>>(dogsJson);
+
+Console.WriteLine(dogs.Count);
+
+
+
+//using (FileStream fs = File.Open(@"C:\Users\erich\MyTestFile.txt", FileMode.Create))
+//using (TextWriter writer = new StreamWriter(fs))
+//{
+//    writer.WriteLine("Avocado");
+//    writer.WriteLine("Grapefruit");
+//}
+    
+//using (FileStream fs = File.OpenRead(@"C:\Users\erich\MyTestFile.txt"))
+//using (TextReader reader = new StreamReader(fs))
+//{
+//    string contents = reader.ReadToEnd();
+//}
+
+//string[] lines = File.ReadAllLines(@"C:\Users\erich\MyTestFile.txt");
+
+Console.WriteLine("Done");
